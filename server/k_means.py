ab84a7930409161824a642d7b377b87e3682f753
Joel Carlsson <joel@Joels-MacBook-Pro.local>
2019-11-22 17:44:11 +0100

    Writing numpy data

--- server/k_means.py
+++ server/k_means.py
@@ -13,10 +13,20 @@ named_data = {label: row for label, row in zip(blog_names, data)}
 
 blog_amount, word_amount = data.shape
 
-k = 4
+# Amount of clusters
+k = 5
 
 # Highest value in np arr. = is word china. Should return 11
 print(np.amax(data[:,0]))
 
 # Lowest value in np arr. = is word china. Should return 0
 print(np.amin(data[:,0]))
+
+
+# The highest amount of times each word exists
+max_frequency = np.amax(data, axis=0)
+# The lowest amount of times each word exists
+min_frequency = np.amin(data, axis=0)
+
+# Hopefully returns array with random integer between max and min frequency between each word
+rand = np.random.randint(min_frequency, max_frequency)
