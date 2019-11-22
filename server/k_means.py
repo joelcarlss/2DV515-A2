6677e7dee5624b82bbac13f10c7ea2f191119ed4
Joel Carlsson <joel@Joels-MacBook-Pro.local>
2019-11-22 19:56:40 +0100

    Trying solutions

--- server/k_means.py
+++ server/k_means.py
@@ -1,15 +1,13 @@
 import numpy as np
 from io import StringIO
 
-
 # Create random places for cluster-thingies
 # rand = np.random.randint(10, size=4)
 
-
-blog_names = np.genfromtxt('blogdata.txt', delimiter='\t', usecols=0, dtype=str)
-data = np.genfromtxt('blogdata.txt', delimiter='\t', skip_header=1)[:,1:]
-named_data = {label: row for label, row in zip(blog_names, data)}
-
+# Create numpy array data from blogdata.txt
+blog_names = np.genfromtxt('blogdata.txt', delimiter='\t', usecols=0, dtype=str) # Names of all blogs(column 0 in doc)
+data = np.genfromtxt('blogdata.txt', delimiter='\t', skip_header=1)[:,1:] # Each blogs word freq. (rows in doc)
+named_data = {label: row for label, row in zip(blog_names, data)} # Pretty presentation (Blog name then word-freq)
 
 blog_amount, word_amount = data.shape
 
@@ -17,16 +15,29 @@ blog_amount, word_amount = data.shape
 k = 5
 
 # Highest value in np arr. = is word china. Should return 11
-print(np.amax(data[:,0]))
+# print(np.amax(data[:,0]))
 
 # Lowest value in np arr. = is word china. Should return 0
-print(np.amin(data[:,0]))
+# print(np.amin(data[:,0]))
 
 
 # The highest amount of times each word exists
 max_frequency = np.amax(data, axis=0)
+
 # The lowest amount of times each word exists
 min_frequency = np.amin(data, axis=0)
 
-# Hopefully returns array with random integer between max and min frequency between each word
-rand = np.random.randint(min_frequency, max_frequency)
+# Mock-values for easier testing
+mock_max = np.array([2, 9, 2, 2, 2])
+mock_min = np.array([0, 5, 0, 0, 0])
+clusters = np.random.randint(mock_min, mock_max+1, (k, len(mock_min)))
+
+# Returns array with random integer between max and min frequency between each word
+# rand = np.random.randint(min_frequency, max_frequency)
+
+# K amount of arrays with random numbers between lowest and highest frequency of each word
+# max_frequency + 1 because random value < max.
+# clusters = np.random.randint(min_frequency, max_frequency+1, (k, word_amount))
+
+print(clusters)
+
