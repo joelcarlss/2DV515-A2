e7492e489096eeb982f057b0b29702af114bcc78
joelcarlss <jc222mw@student.lnu.se>
2019-11-24 16:55:48 +0100

    tying to match all blogs to centroids

--- server/k_means.py
+++ server/k_means.py
@@ -6,10 +6,10 @@ from io import StringIO
 
 # Create numpy array data from blogdata.txt
 blog_names = np.genfromtxt('blogdata.txt', delimiter='\t', usecols=0, dtype=str) # Names of all blogs(column 0 in doc)
-data = np.genfromtxt('blogdata.txt', delimiter='\t', skip_header=1)[:,1:] # Each blogs word freq. (rows in doc)
-named_data = {label: row for label, row in zip(blog_names, data)} # Pretty presentation (Blog name then word-freq)
+blog_data = np.genfromtxt('blogdata.txt', delimiter='\t', skip_header=1)[:, 1:] # Each blogs word freq. (rows in doc)
+named_data = {label: row for label, row in zip(blog_names, blog_data)} # Pretty presentation (Blog name then word-freq)
 
-blog_amount, word_amount = data.shape
+blog_amount, word_amount = blog_data.shape
 
 # Amount of clusters
 k = 5
@@ -22,10 +22,10 @@ k = 5
 
 
 # The highest amount of times each word exists
-max_frequency = np.amax(data, axis=0)
+max_frequency = np.amax(blog_data, axis=0)
 
 # The lowest amount of times each word exists
-min_frequency = np.amin(data, axis=0)
+min_frequency = np.amin(blog_data, axis=0)
 
 # Mock-values for easier testing
 mock_max = np.array([2, 9, 2, 2, 2])
@@ -43,19 +43,23 @@ centroids = np.random.randint(min_frequency, max_frequency+1, (k, word_amount))
 # Writes put pearson for every blog against every centroids.
 # Will remove all values that are similarities between blog-data, and only similarities between clusters and blogs
 # Indexing options explained: [(from start):(to blog_amount)(last k values)]
-clusters = np.array(np.corrcoef(data, centroids)[:blog_amount,-k::])
+clusters = np.array(np.corrcoef(blog_data, centroids)[:blog_amount, -k::])
 
 
-print(np.corrcoef(data[0], centroids)[0][1:])
-print(clusters[0])
+# print(np.corrcoef(blog_data[0], centroids)[0][1:])
+# print(clusters[0])
 
 # The most similar centroid for every blog
 # or.. the index with the highest value on every element in clusters
 matching_centroids = clusters.argmax(axis=1)
 print(matching_centroids)
-print(len(matching_centroids))
 
-test_matching = np.where(matching_centroids == 1)
+# TEST
+# All indexes that matches a specified int
+test_matching = np.where([matching_centroids]) ## TODO: return five arrays where every array is evvery blog that matches the current centroid
 print(test_matching)
+print(len(test_matching))
 
+# All blogs that matches ints in test match
+# blog_data[test_matching]
 
