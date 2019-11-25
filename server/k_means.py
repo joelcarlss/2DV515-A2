3d0e0fb0c274fb409087c68a6b5ca4d10284261a
joelcarlss <jc222mw@student.lnu.se>
2019-11-25 22:20:22 +0100

    algorithm done with room for improvement

--- server/k_means.py
+++ server/k_means.py
@@ -25,6 +25,8 @@ min_frequency = np.amin(blog_data, axis=0)
 # max_frequency + 1 because random value < max.
 centroids = np.random.randint(min_frequency, max_frequency+1, (k, word_amount))
 
+matching_centroids = np.array([])
+
 while True:
 
     # Writes out pearson for every blog against every centroids.
@@ -32,16 +34,21 @@ while True:
     # Indexing options explained: [(from start):(to blog_amount)(last k values)]
     similarities = np.array(np.corrcoef(blog_data, centroids)[:blog_amount, -k::])
 
-    # The most similar centroid for every blog
-    # or.. the index with the highest value on every element in clusters
-    matching_centroids = similarities.argmax(axis=1)
+    if np.array_equal(similarities.argmax(axis=1), matching_centroids):
+        break
+    else:
+        # The most similar centroid for every blog
+        # or.. the index with the highest value on every element in clusters
+        matching_centroids = similarities.argmax(axis=1)
 
     # All indexes that matches a specified int
     for i in range(k):  # For each centroid, get all blogs and create an new centroid in the middle
         best_matching_indexes = np.where(matching_centroids == i)[0]  # Indexes that is most similar with centroid i
         best_matching_blogs = blog_data[best_matching_indexes]  # Blogs that are most similar with centroid i
         centroids[i] = np.sum(best_matching_blogs, axis=0) / len(best_matching_indexes)  # Average blog. = new centroid
-    break
+
+    print('matching_centroids')
+
 
 
 print("----------------------------------------")
